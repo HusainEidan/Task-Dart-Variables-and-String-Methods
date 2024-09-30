43bd27a0712fbad3f2c47d7920098b854ae79feb
HusainEidan <h.m.abbas1999@gmail.com>
2024-09-30 01:38:07 -0700

    Task111

---
+++ main.dart
@@ -0,0 +1,23 @@
+
+
+void main(){
+String firstname = "Husain";
+String lastname = "Eidan" ;
+int age = 25 ;
+double height = 173 ;
+bool Married = true ;
+String drink = 'juice';
+String flavor = 'orange';
+int number = 5;
+int result;
+
+
+print("The temperature is 20C ");
+print("I like $flavor $drink");
+result = number+number;
+print("$number+$number=$result");
+print("My firstname is $firstname and my lastname length is ${lastname.length}");
+
+
+
+}
